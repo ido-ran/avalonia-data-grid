077a5d5a9785fe5c48daf4524d2d71f1b156c9af
Ido Ran <ido.ran@stratasys.com>
2019-09-04 09:59:27 +0300

    try to use DataGrid

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Markup.Xaml;
+
+namespace ParametersEditor2
+{
+    public class App : Application
+    {
+        public override void Initialize()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+   }
+}
